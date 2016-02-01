e424a285eb7d326bae8b2a7948778c323d0c29e2
bsmukasa <bsmukasa@gmail.com>
2016-02-01 16:37:41 -0500

    Add BondViewSet

--- bond_analytics_project/bondapi/views.py
+++ bond_analytics_project/bondapi/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from models import Bond
+from serializers import BondSerializer
+
+
+class BondViewSet(ModelViewSet):
+    serializer_class = BondSerializer
+    queryset = Bond.objects.all()
