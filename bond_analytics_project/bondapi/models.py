a7c1c5328966882dae51732ec69cc3102b2ac5b2
bsmukasa <bsmukasa@gmail.com>
2016-02-05 16:09:25 -0500

    Minor refactoring.

--- bond_analytics_project/bondapi/models.py
+++ bond_analytics_project/bondapi/models.py
@@ -9,41 +9,21 @@ DAYS_PER_YEAR = 364.2425
 
 
 class Bond(models.Model):
-    """A Django model class for a Bond financial instrument.
-
-    A bond is a financial instrument issued by the government or corporations when they need to
-    borrow money from the public on a long term basis to finance certain projects. Interest payments
-    called coupons are typically paid out to bond holders on a regular basis while the entire loan
-    amount called the Face or Par value is repaid at the end.
-
-    Attributes:
-        name: The name of the bond.
-        face_value: The principal of the bond to be repaid at the end of the maturity period.
-        annual_coupon_rate: The stated annual interest rate for a bond.
-        annual_payment_frequency: The number of coupon interest payments made annually.
-        annual_interest: The dollar amount of interest earned annually.
-
-        issue_date: The date the bond was issued.
-        settlement_date: The date the bond is purchased.
-        maturity_date: The date the bond matures.
-        term_to_maturity: The number of years before the bond matures.
-
-    """
     name = models.CharField(max_length=128)
     face_value = models.DecimalField(max_digits=20, decimal_places=2)
-    annual_coupon_rate = models.DecimalField(max_digits=5, decimal_places=4)
-    annual_required_return = models.DecimalField(max_digits=5, decimal_places=4)
+    annual_coupon_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    annual_required_return = models.DecimalField(max_digits=5, decimal_places=2)
     annual_payment_frequency = models.IntegerField()
 
-    semi_annual_coupon_payment = models.DecimalField(max_digits=10, decimal_places=4)
-    bond_price = models.DecimalField(max_digits=10, decimal_places=4)
-    bond_valuation = models.DecimalField(max_digits=10, decimal_places=4)
+    semi_annual_coupon_payment = models.DecimalField(max_digits=10, decimal_places=2)
+    bond_price = models.DecimalField(max_digits=10, decimal_places=2)
+    bond_valuation = models.DecimalField(max_digits=10, decimal_places=2)
 
     issue_date = models.DateField()
     settlement_date = models.DateField()
     maturity_date = models.DateField()
-    term_to_maturity = models.DecimalField(max_digits=6, decimal_places=2)
-    periods_to_maturity = models.DecimalField(max_digits=6, decimal_places=2)
+    term_to_maturity = models.DecimalField(max_digits=5, decimal_places=2)
+    periods_to_maturity = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return '{}'.format(self.name)
